a28c0e3abc0bb6f7f1b3bfc69dcf9731506bbdf7
Xavier Abril <xaiken99@gmail.com>
2025-10-24 17:27:21 +0200

    Aggiunta leaflet per davvero

--- AppSegnalazioni/src/app/componenti/tabs-component/tabs-component.spec.ts
+++ AppSegnalazioni/src/app/componenti/tabs-component/tabs-component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ToolbarComponent } from './toolbar-component';
+import { TabsComponent } from './tabs-component';
 
-describe('ToolbarComponent', () => {
-  let component: ToolbarComponent;
-  let fixture: ComponentFixture<ToolbarComponent>;
+describe('TabsComponent', () => {
+  let component: TabsComponent;
+  let fixture: ComponentFixture<TabsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ToolbarComponent]
+      imports: [TabsComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ToolbarComponent);
+    fixture = TestBed.createComponent(TabsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ AppSegnalazioni/src/app/componenti/tabs-component/tabs-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-component',
+  imports: [],
+  templateUrl: './tabs-component.html',
+  styleUrl: './tabs-component.scss',
+})
+export class TabsComponent {
+
+}
