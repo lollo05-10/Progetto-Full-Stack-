1d0238351c52b074e738878959c4f79718be3872
Rifti Nafsir <rifti.nafsir.14-1@accademiadigitaleliguria.it>
2025-10-27 09:20:39 +0100

    aggiunta mappa

--- AppSegnalazioni/src/app/componenti/map-component/map-component.ts
+++ AppSegnalazioni/src/app/componenti/map-component/map-component.ts
@@ -9,10 +9,26 @@ import * as L from 'leaflet';
 })
 export class MapComponent {
   private map: L.Map | undefined;
+  
   ngAfterViewinit() {
     this.setupMap();
   }
+  
+async setupMap() {
+    this.map = L.map('map');
 
+    this.map.setView([44.40614435613236, 8.949400422559357], 13);
+
+    const tileLayer = L.tileLayer(
+      'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+
+    tileLayer.addTo(this.map);
+  }
 }
-
 
