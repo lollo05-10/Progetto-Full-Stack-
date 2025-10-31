ec88e3490c7648ca6f7f47a34f78a8e19972eadb
Xavier Abril <xaiken99@gmail.com>
2025-10-31 15:40:41 +0100

    bottom bar

--- AppSegnalazioni/src/app/componenti/tabs-component/tabs-component.ts
+++ AppSegnalazioni/src/app/componenti/tabs-component/tabs-component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTabsModule} from '@angular/material/tabs';
+import { MatDividerModule } from '@angular/material/divider';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-tabs-component',
-  imports: [MatTabsModule, MatIconModule],
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, MatDividerModule, RouterLink],
   templateUrl: './tabs-component.html',
   styleUrl: './tabs-component.scss',
 })
