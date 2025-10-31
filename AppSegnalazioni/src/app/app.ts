ffb38f3dd29679d7349df61e443a7e6e6aa4d1dd
Rifti Nafsir <rifti.nafsir.14-1@accademiadigitaleliguria.it>
2025-10-31 14:24:15 +0100

    heaader e folder service e model

--- AppSegnalazioni/src/app/app.ts
+++ AppSegnalazioni/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import 'leaflet/dist/leaflet.css';
 import { TabsComponent } from './componenti/tabs-component/tabs-component';
+import { HeaderComponent } from "./componenti/header-component/header-component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TabsComponent],
+  imports: [RouterOutlet, TabsComponent, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
