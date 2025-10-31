d9f7f23e20d03d11b6ec5193c6da6d05962a1259
Rifti Nafsir <rifti.nafsir.14-1@accademiadigitaleliguria.it>
2025-10-31 16:21:49 +0100

    filter icona

--- AppSegnalazioni/src/app/componenti/buttons-component/buttons-component.ts
+++ AppSegnalazioni/src/app/componenti/buttons-component/buttons-component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIcon } from "@angular/material/icon";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-buttons-component',
-  imports: [MatIcon],
+  imports: [MatIcon, RouterLink],
   templateUrl: './buttons-component.html',
   styleUrl: './buttons-component.scss',
 })
