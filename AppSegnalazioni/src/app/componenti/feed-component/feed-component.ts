f9b86f13ffd9389a4fb694ee4821263dcb759125
Rifti Nafsir <rifti.nafsir.14-1@accademiadigitaleliguria.it>
2025-11-06 14:55:08 +0100

    feed css changes

--- AppSegnalazioni/src/app/componenti/feed-component/feed-component.ts
+++ AppSegnalazioni/src/app/componenti/feed-component/feed-component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from "@angular/material/icon";
 import { ButtonsComponent } from "../buttons-component/buttons-component";
+import { ReportCardComponent } from "../report-card-component/report-card-component";
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: 'app-feed-component',
-  imports: [MatIconModule, ButtonsComponent],
+  imports: [MatIconModule, ButtonsComponent, ReportCardComponent, MatCardModule],
   templateUrl: './feed-component.html',
   styleUrl: './feed-component.scss'
 })
--- AppSegnalazioni/src/app/componenti/report-card-component/report-card-component.ts
+++ AppSegnalazioni/src/app/componenti/report-card-component/report-card-component.ts
@@ -3,10 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Report } from '../../models/report';
 import { RouterLink } from '@angular/router';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-report-card',
-  imports: [MatCardModule, MatButtonModule,RouterLink],
+  imports: [MatCardModule, MatButtonModule, RouterLink, MatIcon],
   templateUrl: './report-card-component.html',
   styleUrl: './report-card-component.scss'
 })
