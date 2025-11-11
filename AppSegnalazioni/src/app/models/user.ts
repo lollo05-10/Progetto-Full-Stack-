240d06b4d55a2b6a7bec70780333b826b26e7b0d
aichasam29 <aicha.samiih@gmail.com>
2025-11-11 16:00:26 +0100

    profil+registrazione

---
+++ AppSegnalazioni/src/app/models/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: number;
+    username : string;
+    gender:string;
+    dob:string;
+    isAdmin : boolean;
+
+}
+
--- AppSegnalazioni/src/app/services/dataservice/dataservice.ts
+++ AppSegnalazioni/src/app/services/dataservice/dataservice.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { FeatureCollection } from 'geojson';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../../models/user';
+import { Report } from '../../models/report';
+
 
 export interface AppReport {
   id: number;
@@ -14,6 +17,7 @@ export interface AppReport {
   longitude: number;
   distance?: number;
 }
+
 @Injectable({
   providedIn: 'root',
 })
@@ -52,4 +56,59 @@ export class DataService {
   postReport(report: AppReport): Observable<AppReport> {
     return this.http.post<AppReport>(this.apiUrl, report);
   }
+
+
+ // data-service.ts
+registerUser(userData: any): Promise<any> {
+  return fetch('http://localhost:5077/api/user', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: userData.username,
+      password: userData.password,
+      gender: userData.gender,
+      dob: userData.dob,
+      isAdmin: false
+    })
+  })
+  .then(resp => {
+    if (!resp.ok) {
+      throw new Error('Errore durante la registrazione');
+    }
+    return resp.json();
+  })
+  .catch(err => {
+    console.error('Errore registerUser:', err);
+    throw err;
+  });
+}
+
+  // Simula l'ID dell'utente loggato (per ora fisso)
+
+  getCurrentUserId(): Promise<number> {
+  return Promise.resolve(1);
+}
+
+getUserById(userId: number): Promise<User> {
+  return fetch(`http://localhost:5077/api/user/${userId}`)
+    .then(resp => resp.json())
+    .catch(err => console.error(err));
+}
+
+  
+
+getUserReports(userId: number): Promise<Report[]> {
+  return fetch(`/api/user/${userId}/reports`)
+    .then(resp => resp.json())
+    .then((reports: Report[]) => reports) 
+    .catch(err => {
+      console.error(err);
+      return []; 
+    });
+
+
+}
+
 }
